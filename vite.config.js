6d90d03ed156e653d36a56323f48bd00e58587ce
kevinaltschuler <kevinaltschuler@gmail.com>
2022-01-13 22:49:57 -0500

    another fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import commonjs from 'rollup-plugin-commonjs';
+
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+import react from 'react'
+import reactDom from 'react-dom';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), rollupNodePolyFill()]
+  plugins: [commonjs({
+    include: 'node_modules/**',
+    namedExports: {
+      react: Object.keys(react),
+      'react-dom': Object.keys(reactDom)
+    }
+  }), rollupNodePolyFill()]
 })
