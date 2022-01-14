eda26a0c5f637a62fbc79aced8807431e0a5e48b
kevinaltschuler <kevinaltschuler@gmail.com>
2022-01-13 22:59:44 -0500

    fuck vite, my packages huse built in node modules and i dont wanna deal with polyfilling

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello parcel, fuck vite!</p>
         <p>
           <button type="button" onClick={() => makeRequest()}>
             count is: {count}
@@ -33,25 +33,6 @@ function App() {
         </p>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
